bd9e4577291911ab9e7c606667e07f49206e6db2
ParisaRashidi <parisaRashi>
2016-01-25 02:10:55 +0330

    user interface created for userProfileActivity

--- app/src/main/java/ir/rastanco/mobilemarket/presenter/UserProfilePresenter/UserInformation.java
+++ app/src/main/java/ir/rastanco/mobilemarket/presenter/UserProfilePresenter/UserInformation.java
@@ -1,6 +1,8 @@
 package ir.rastanco.mobilemarket.presenter.UserProfilePresenter;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -10,7 +12,9 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 
 import ir.rastanco.mobilemarket.R;
@@ -28,71 +32,85 @@ public class UserInformation extends Fragment {
     private Security sec;
     private String user;
     private String pass;
-
+    private Typeface font;
+    private Button btnLogin;
+    private Button btnSignUp;
+    private Button btnForgotPass;
+    private EditText username;
+    private EditText password;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        final View userProfileView= inflater.inflate(R.layout.fragment_user_information, container, false);
-
-        Configuration.UserInformationFragment=getContext();
-        sch=new ServerConnectionHandler(Configuration.UserInformationFragment);
-        Button btnLogin=(Button) userProfileView.findViewById(R.id.btn_login);
-        Button btnSignUp=(Button) userProfileView.findViewById(R.id.btn_signup);
-        Button btnForgotPass=(Button) userProfileView.findViewById(R.id.btn_forgot);
-
-        final EditText username=(EditText) userProfileView.findViewById(R.id.et_username);
-        final EditText password=(EditText) userProfileView.findViewById(R.id.et_password);
-
-        sec=new Security();
-
+         final View userProfileView= inflater.inflate(R.layout.fragment_user_information, container, false);
+         font = Typeface.createFromAsset(getActivity().getAssets(),"fonts/yekan_font.ttf");
+         Configuration.UserInformationFragment=getContext();
+         sch=new ServerConnectionHandler(Configuration.UserInformationFragment);
+         btnLogin=(Button) userProfileView.findViewById(R.id.btn_login);
+         btnSignUp=(Button) userProfileView.findViewById(R.id.btn_enter);
+         btnForgotPass=(Button) userProfileView.findViewById(R.id.btn_forgot);
+         username=(EditText) userProfileView.findViewById(R.id.et_username);
+         password=(EditText) userProfileView.findViewById(R.id.et_password);
+         sec=new Security();
+         this.setYekanFont();
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                UserInfo aUser=new UserInfo();
-                String key=sch.GetKey("http://decoriss.com/json/get,com=auth");
-                //String key="974401741";
-                //user= String.valueOf(username.getText());
-                user="mahdavikia.m@gmail.com";
-                aUser.setUserEmail(user);
-                //pass= String.valueOf(password.getText());
-                pass="1234";
-                String hashInfo=sec.encode(user,pass,key);
-                ArrayList<String> response=new ArrayList<String>();
-                response=sch.GetAuthorizeResponse(hashInfo,key);
-                Log.d("Response:",response.get(0));
-                if(response.get(0).equals("")){
-                    aUser.setUserId(Integer.parseInt(response.get(1)));
-                    aUser.setUserLoginStatus(1);
-                    sch.addUserInfoToTable(aUser);
-                    Configuration.UserProfileViewPager.setCurrentItem(1);
-                }
+               btnLogin.setTextColor(Color.RED);
+
+//                UserInfo aUser=new UserInfo();
+//                String key=sch.GetKey("http://decoriss.com/json/get,com=auth");
+//                //String key="974401741";
+//                //user= String.valueOf(username.getText());
+//                user="mahdavikia.m@gmail.com";
+//                aUser.setUserEmail(user);
+//                //pass= String.valueOf(password.getText());
+//                pass="1234";
+//                String hashInfo=sec.encode(user,pass,key);
+//                ArrayList<String> response=new ArrayList<String>();
+//                response=sch.GetAuthorizeResponse(hashInfo,key);
+//                Log.d("Response:",response.get(0));
+//                if(response.get(0).equals("")){
+//                    aUser.setUserId(Integer.parseInt(response.get(1)));
+//                    aUser.setUserLoginStatus(1);
+//                    sch.addUserInfoToTable(aUser);
+//                    Configuration.UserProfileViewPager.setCurrentItem(1);
+//                }
             }
         });
 
         btnSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                Intent intent = new Intent();
-                intent.setAction(Intent.ACTION_VIEW);
-                intent.addCategory(Intent.CATEGORY_BROWSABLE);
-                intent.setData(Uri.parse("http://decoriss.com/register,ثبت-نام_"));
-                userProfileView.getContext().startActivity(intent);
+                 btnSignUp.setTextColor(Color.RED);
+//                Intent intent = new Intent();
+//                intent.setAction(Intent.ACTION_VIEW);
+//                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+//                intent.setData(Uri.parse("http://decoriss.com/register,ثبت-نام_"));
+//                userProfileView.getContext().startActivity(intent);
             }
         });
 
         btnForgotPass.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                Intent intent = new Intent();
-                intent.setAction(Intent.ACTION_VIEW);
-                intent.addCategory(Intent.CATEGORY_BROWSABLE);
-                intent.setData(Uri.parse("http://decoriss.com/forgetpassword,فراموشی-رمز-عبور_"));
-                userProfileView.getContext().startActivity(intent);
+                btnForgotPass.setTextColor(Color.RED);
+//                Intent intent = new Intent();
+//                intent.setAction(Intent.ACTION_VIEW);
+//                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+//                intent.setData(Uri.parse("http://decoriss.com/forgetpassword,فراموشی-رمز-عبور_"));
+//                userProfileView.getContext().startActivity(intent);
             }
         });
 
         return userProfileView;
+    }
+
+    private void setYekanFont() {
+        btnLogin.setTypeface(font);
+        btnSignUp.setTypeface(font);
+        btnForgotPass.setTypeface(font);
+        username.setTypeface(font);
+        password.setTypeface(font);
+
     }
 }
