50bc5298f84d3a91526efadf02b0a17c37be651f
ParisaRashidi <parisaRashidiNezhad@gmail.com>
2016-02-01 11:14:10 +0330

    create golden rating bars

--- app/src/main/java/ir/rastanco/mobilemarket/presenter/shoppingBagPresenter/shoppingBagAdapter.java
+++ app/src/main/java/ir/rastanco/mobilemarket/presenter/shoppingBagPresenter/shoppingBagAdapter.java
@@ -10,6 +10,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -34,6 +35,10 @@ public class shoppingBagAdapter extends ArrayAdapter<Integer> {
     private Product aProduct;
     private ServerConnectionHandler sch;
     private Spinner spinnerCounter;
+    private String spinnerValueInString;
+    private int spinnerValueInInteger;
+    EditText spinnerValue;
+    private Typeface trafficFont;
 
     public shoppingBagAdapter(Context context, int resource, ArrayList<Integer> productsId) {
         super(context, resource, productsId);
@@ -47,16 +52,21 @@ public class shoppingBagAdapter extends ArrayAdapter<Integer> {
 
         LayoutInflater inflater = myContext.getLayoutInflater();
         final View rowView = inflater.inflate(R.layout.shopping_bag_item, null);
-
         aProduct=new Product();
         aProduct=sch.getAProduct(selectedProducts.get(position));
         spinnerCounter = (Spinner)rowView.findViewById(R.id.spinner_counter);
+        spinnerValueInString = spinnerCounter.getSelectedItem().toString();
+        spinnerValueInInteger = Integer.parseInt(spinnerValueInString);
+        //spinnerValue = (EditText)rowView.findViewById(R.id.spinner_value);
+        //spinnerValue.setText(spinnerValueInString);
+        //spinnerValue.setTypeface(trafficFont);
         ImageView imgProduct=(ImageView)rowView.findViewById(R.id.img_productImage);
         TextView txtProductTitle=(TextView) rowView.findViewById(R.id.txt_productTitle);
         TextView txtProductPrice=(TextView) rowView.findViewById(R.id.txt_productPrice);
         TextView txtProductOff=(TextView) rowView.findViewById(R.id.txt_productOff);
         Button btnDelete=(Button)rowView.findViewById(R.id.imb_delete);
         TextView totalPrice = (TextView)rowView.findViewById(R.id.txt_total_price);
+
         btnDelete.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -99,7 +109,7 @@ public class shoppingBagAdapter extends ArrayAdapter<Integer> {
                 "&size="+
                 Configuration.articleDisplaySize+"x"+Configuration.articleDisplaySize+
                 "&q=30";
-        Typeface trafficFont = Typeface.createFromAsset(getContext().getAssets(),"fonts/B Traffic.ttf");
+        trafficFont = Typeface.createFromAsset(getContext().getAssets(),"fonts/B Traffic.ttf");
         imgLoader.DisplayImage(image_url_1, imgProduct);
 
 
@@ -107,10 +117,10 @@ public class shoppingBagAdapter extends ArrayAdapter<Integer> {
 
         //Price and off Set
         int finalPrice=0;
-        int off=0;
+        int off = 0;
         if(aProduct.getPriceOff()!=0)
-            off=(aProduct.getPrice()*aProduct.getPriceOff())/100;
-        finalPrice=aProduct.getPrice()-off;
+            off=((aProduct.getPrice()*aProduct.getPriceOff())/100);
+            finalPrice=(aProduct.getPrice()-off);
         ///Price
         String numberProductPrice = String.valueOf(aProduct.getPrice());
         double amount = Double.parseDouble(numberProductPrice);
