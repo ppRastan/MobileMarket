59669b3615208c63e91770db63d1ceb92fd9225f
ParisaRashidi <parisaRashidiNezhad@gmail.com>
2016-02-01 14:41:14 +0330

    size of raging bars become smaller

--- app/src/main/java/ir/rastanco/mobilemarket/presenter/ProductInfoPresenter/ProductInfoActivity.java
+++ app/src/main/java/ir/rastanco/mobilemarket/presenter/ProductInfoPresenter/ProductInfoActivity.java
@@ -3,9 +3,15 @@ package ir.rastanco.mobilemarket.presenter.ProductInfoPresenter;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.LayerDrawable;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.widget.RatingBar;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -22,13 +28,14 @@ public class ProductInfoActivity extends Activity {
     private ArrayList<Product> allProducts;
     private LayoutInflater inflater;
     private ViewPager viewPager;
-
+    private float y1, y2;
+    private MotionEvent touchEvent;
+    private RatingBar customRatingBar;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.swip_product_gallery);
         Configuration.ProductInfoActivity = this;
-
         Intent intent = this.getIntent();
         Bundle productBundle=new Bundle();
         productBundle=intent.getExtras();
@@ -38,7 +45,42 @@ public class ProductInfoActivity extends Activity {
         viewPager=(ViewPager)findViewById(R.id.pager);
         viewPager.setAdapter(new FullScreenImageAdapter(this, allProducts, allProducts.size()));
         viewPager.setCurrentItem(intent.getIntExtra("position", 0));
+        customRatingBar = (RatingBar)findViewById(R.id.rank_of_product);
+
     }
 
+    public boolean onTouchEvent(MotionEvent touchevent)
+    {
+        switch (touchevent.getAction())
+        {
+            case MotionEvent.ACTION_DOWN:
+            {
+                y1 = touchevent.getY();
+                break;
+            }
+            case MotionEvent.ACTION_UP:
+            {
+                y2 = touchevent.getY();
+
+                if (y1 < y2)
+                {
+//                    Intent intentProductInfo = new Intent(viewLayout.getContext(),ProductOptionActivity.class);
+//                    intentProductInfo.putExtra("productId", products.get(position).getId());
+//                    intentProductInfo.putExtra("groupId", products.get(position).getGroupId());
+//                    viewLayout.getContext().startActivity(intentProductInfo);
+                    Toast.makeText(getApplicationContext(),"swipeDown",Toast.LENGTH_LONG).show();
+                }
+
+                if (y1 > y2)
+                {
+                    Toast.makeText(getApplicationContext(),"swipeDown",Toast.LENGTH_LONG).show();
+
+                }
+
+                break;
+            }
+        }
+        return false;
+    }
 }
 
