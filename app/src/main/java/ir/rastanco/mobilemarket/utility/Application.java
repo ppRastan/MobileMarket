bad04bf8436c402d30f5cb8bc8f966d18af23980
ParisaRashidi <parisaRashidiNezhad@gmail.com>
2016-02-03 17:25:28 +0330

    test yekan font on application

--- app/src/main/java/ir/rastanco/mobilemarket/utility/Application.java
+++ app/src/main/java/ir/rastanco/mobilemarket/utility/Application.java
@@ -1,5 +1,5 @@
 package ir.rastanco.mobilemarket.utility;
-
+//Created by parisaRashidiNezhad
 public final class Application extends android.app.Application {
     @Override
     public void onCreate() {
