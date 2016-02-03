917051bee083bddb9d31d4077f1edc543f45f437
ParisaRashidi <parisaRashidiNezhad@gmail.com>
2016-02-03 17:24:52 +0330

    test yekan font on application

--- app/src/main/java/ir/rastanco/mobilemarket/utility/Application.java
+++ app/src/main/java/ir/rastanco/mobilemarket/utility/Application.java
@@ -5,7 +5,7 @@ public final class Application extends android.app.Application {
     public void onCreate() {
         super.onCreate();
         FontsOverride.setDefaultFont(this, "DEFAULT", "fonts/bnazanin.ttf");
-        FontsOverride.setDefaultFont(this, "MONOSPACE","fonts/B Roya.ttf");
+        FontsOverride.setDefaultFont(this, "MONOSPACE","fonts/Yekan_3.ttf");
         FontsOverride.setDefaultFont(this, "SERIF", "fonts/bnazanin.ttf");
         FontsOverride.setDefaultFont(this, "SANS_SERIF", "fonts/bnazanin.ttf");
     }
