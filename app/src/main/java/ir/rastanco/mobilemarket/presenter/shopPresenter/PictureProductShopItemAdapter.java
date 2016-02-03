e3db7274030522d71fac66ae015eb808a4aff84a
ParisaRashidi <parisaRashidiNezhad@gmail.com>
2016-02-03 21:43:59 +0330

    kamran font added

--- app/src/main/java/ir/rastanco/mobilemarket/presenter/shopPresenter/PictureProductShopItemAdapter.java
+++ app/src/main/java/ir/rastanco/mobilemarket/presenter/shopPresenter/PictureProductShopItemAdapter.java
@@ -57,7 +57,7 @@ public class PictureProductShopItemAdapter extends BaseAdapter{
         inflater = ( LayoutInflater )context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         allProduct =products;
         sch=new ServerConnectionHandler(Configuration.superACFragment);
-        yekanFont = Typeface.createFromAsset(context.getAssets(),"fonts/B Nazanin_p30download.com.ttf");
+        yekanFont = Typeface.createFromAsset(context.getAssets(),"fonts/B Kamran Bold_p30download.com.ttf");
 
     }
     public PictureProductShopItemAdapter(Context context)
