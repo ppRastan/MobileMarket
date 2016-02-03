631856c493a0e10044654e87bed7958b005d91e2
ParisaRashidi <parisaRashidiNezhad@gmail.com>
2016-02-03 17:51:36 +0330

    upgrate icon added offer icon added test koodak font

--- app/src/main/java/ir/rastanco/mobilemarket/utility/Application.java
+++ app/src/main/java/ir/rastanco/mobilemarket/utility/Application.java
@@ -5,7 +5,7 @@ public final class Application extends android.app.Application {
     public void onCreate() {
         super.onCreate();
         FontsOverride.setDefaultFont(this, "DEFAULT", "fonts/bnazanin.ttf");
-        FontsOverride.setDefaultFont(this, "MONOSPACE","fonts/Yekan_3.ttf");
+        FontsOverride.setDefaultFont(this, "MONOSPACE","fonts/B Koodak Bold_p30download.com.ttf");
         FontsOverride.setDefaultFont(this, "SERIF", "fonts/bnazanin.ttf");
         FontsOverride.setDefaultFont(this, "SANS_SERIF", "fonts/bnazanin.ttf");
     }
