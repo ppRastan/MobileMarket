545e860e467467c65104926f10e43c566f54a467
shaisteS <shs.woem@gmail.com>
2016-02-20 12:24:40 +0330

    add splash screen

---
+++ app/src/main/java/ir/rastanco/mobilemarket/presenter/SplashScreen.java
@@ -0,0 +1,60 @@
+package ir.rastanco.mobilemarket.presenter;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MotionEvent;
+
+import ir.rastanco.mobilemarket.R;
+import ir.rastanco.mobilemarket.dataModel.serverConnectionModel.ServerConnectionHandler;
+
+/**
+ * Created by shaisteS on 1394/12/1.
+ */
+public class SplashScreen extends Activity {
+
+    private Thread mSplashThread;
+    private ServerConnectionHandler sch;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+        final SplashScreen sPlashScreen = this;
+        mSplashThread =  new Thread(){
+            @Override
+            public void run(){
+                try {
+                    synchronized(this){
+                        // Wait given period of time or exit on touch
+                        wait(1000);
+                    }
+                }
+                catch(InterruptedException ex){
+                }
+                finish();
+                Intent intent = new Intent();
+                intent.setClass(sPlashScreen, MainActivity.class);
+                startActivity(intent);
+            }
+        };
+
+        mSplashThread.start();
+    }
+
+    /**
+     * Processes splash screen touch events
+     */
+    @Override
+    public boolean onTouchEvent(MotionEvent evt)
+    {
+        if(evt.getAction() == MotionEvent.ACTION_DOWN)
+        {
+            synchronized(mSplashThread){
+                mSplashThread.notifyAll();
+            }
+        }
+        return true;
+    }
+
+}
