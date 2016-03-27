df17d7f51267e7887f74e1fc9c28f09057550e95
shaisteS <shs.woem@gmail.com>
2016-03-27 11:52:00 +0430

    change DataBaseHandler to singleton class

--- app/src/main/java/ir/rastanco/mobilemarket/dataModel/dataBaseConnectionModel/DataBaseHandler.java
+++ app/src/main/java/ir/rastanco/mobilemarket/dataModel/dataBaseConnectionModel/DataBaseHandler.java
@@ -107,9 +107,6 @@ public class DataBaseHandler  extends SQLiteOpenHelper {
 
     public static synchronized DataBaseHandler getInstance(Context context) {
 
-        // Use the application context, which will ensure that you
-        // don't accidentally leak an Activity's context.
-        // See this article for more information: http://bit.ly/6LRzfx
         if (sInstance == null) {
             sInstance = new DataBaseHandler(context.getApplicationContext());
         }
