5053acd50feb2204557d46b6b8ae78f084c14db3
ParisaRashidi <parisaRashidiNezhad@gmail.com>
2016-03-29 12:30:59 +0430

    share icon changed ctrl + shift +f pressed for clearing xml files all strings are in string.xml file

--- app/src/main/java/ir/rastanco/mobilemarket/utility/Utilities.java
+++ app/src/main/java/ir/rastanco/mobilemarket/utility/Utilities.java
@@ -2,6 +2,8 @@ package ir.rastanco.mobilemarket.utility;
 
 import java.util.ArrayList;
 
+import ir.rastanco.mobilemarket.R;
+
 /**
  * Created by ShaisteS on 1395/1/8.
  * This class include utility Method
@@ -19,19 +21,19 @@ public class Utilities {
 
     public ArrayList<String> getPriceFilterTitle(){
         ArrayList<String> priceFilter = new ArrayList<String>();
-        priceFilter.add("تا سقف 1 میلیون تومان");
-        priceFilter.add("تا سقف 5 میلیون تومان");
-        priceFilter.add("تا سقف 10 میلیون تومان");
-        priceFilter.add("بالاتر از 10 میلیون تومان");
+        priceFilter.add(Configuration.getConfig().MainActivityContext.getResources().getString(R.string.upto1million));
+        priceFilter.add(Configuration.getConfig().MainActivityContext.getResources().getString(R.string.upto5million));
+        priceFilter.add(Configuration.getConfig().MainActivityContext.getResources().getString(R.string.upto10million));
+        priceFilter.add(Configuration.getConfig().MainActivityContext.getResources().getString(R.string.over10million));
         return priceFilter;
     }
     public int convertPriceTitleToInt(String priceTitle){
         int price=0;
-        if(priceTitle.equals("تا سقف 1 میلیون تومان"))
+        if(priceTitle.equals(Configuration.getConfig().MainActivityContext.getResources().getString(R.string.upto1million)))
             price=1000000;
-        else if (priceTitle.equals("تا سقف 5 میلیون تومان"))
+        else if (priceTitle.equals(Configuration.getConfig().MainActivityContext.getResources().getString(R.string.upto5million)))
             price=5000000;
-        else if (priceTitle.equals("تا سقف 10 میلیون تومان"))
+        else if (priceTitle.equals(Configuration.getConfig().MainActivityContext.getResources().getString(R.string.upto10million)))
             price=getAtLeastHighestPrice();
         else
             price=10000001; //1 is sign for price is above
