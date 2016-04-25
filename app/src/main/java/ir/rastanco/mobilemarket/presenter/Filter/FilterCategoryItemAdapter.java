5d8ec97c4b178b8e044b57af0d9999fd856f247c
ParisaRashidi <parisaRashidiNezhad@gmail>
2016-04-25 10:56:33 +0430

    font has been set for dialogue  group

--- app/src/main/java/ir/rastanco/mobilemarket/presenter/Filter/FilterCategoryItemAdapter.java
+++ app/src/main/java/ir/rastanco/mobilemarket/presenter/Filter/FilterCategoryItemAdapter.java
@@ -14,7 +14,6 @@ import java.util.ArrayList;
 import ir.rastanco.mobilemarket.R;
 import ir.rastanco.mobilemarket.dataModel.Category;
 import ir.rastanco.mobilemarket.dataModel.serverConnectionModel.ServerConnectionHandler;
-import ir.rastanco.mobilemarket.utility.PriceUtility;
 
 /**
  * Created by ShaisteS on 1395/1/27.
@@ -24,11 +23,11 @@ public class FilterCategoryItemAdapter extends ArrayAdapter<Integer> {
     private ArrayList<Integer> categoriesId;
     private ServerConnectionHandler serverConnectionHandler;
 
-    public FilterCategoryItemAdapter(Context context,int resource, ArrayList<Integer> categoriesId) {
-        super(context,resource,categoriesId);
-        this.context =(Activity) context;
+    public FilterCategoryItemAdapter(Context context, int resource, ArrayList<Integer> categoriesId) {
+        super(context, resource, categoriesId);
+        this.context = (Activity) context;
         this.categoriesId = categoriesId;
-        serverConnectionHandler=ServerConnectionHandler.getInstance(context);
+        serverConnectionHandler = ServerConnectionHandler.getInstance(context);
     }
 
     static class ViewHolder {
@@ -40,29 +39,28 @@ public class FilterCategoryItemAdapter extends ArrayAdapter<Integer> {
 
     public View getView(final int position, View convertView, ViewGroup parent) {
         ViewHolder holder;
-        if (convertView==null){
+        if (convertView == null) {
             LayoutInflater inflater = context.getLayoutInflater();
             convertView = inflater.inflate(R.layout.dialog_filter_item, parent, false);
-            holder=new ViewHolder(); convertView.setTag(holder);
+            holder = new ViewHolder();
+            convertView.setTag(holder);
             holder.txtCategoryName=(TextView)convertView.findViewById(R.id.txt_Name);
-            holder.txtCategoryName= PriceUtility.getInstance().changeFontToYekan(holder.txtCategoryName,context);
-            holder.imgNextIcon=(ImageButton)convertView.findViewById(R.id.imb_next);
+            //holder.txtCategoryName= PriceUtility.getInstance().changeFontToYekan(holder.txtCategoryName,context);
+            holder.imgNextIcon = (ImageButton) convertView.findViewById(R.id.imb_next);
         } else
             holder = (ViewHolder) convertView.getTag();
 
-        if (categoriesId.get(position)==0){
+        if (categoriesId.get(position) == 0) {
             holder.imgNextIcon.setVisibility(View.INVISIBLE);
             holder.txtCategoryName.setText(context.getString(R.string.all));
 
-        }
-        else {
+        } else {
 
-            Category aCategory=serverConnectionHandler.getACategoryWithId(categoriesId.get(position));
+            Category aCategory = serverConnectionHandler.getACategoryWithId(categoriesId.get(position));
 
-            if(aCategory.getHasChild()==0){
+            if (aCategory.getHasChild() == 0) {
                 holder.imgNextIcon.setVisibility(View.INVISIBLE);
-            }
-            else
+            } else
                 holder.imgNextIcon.setVisibility(View.VISIBLE);
 
             holder.txtCategoryName.setText(aCategory.getTitle());
