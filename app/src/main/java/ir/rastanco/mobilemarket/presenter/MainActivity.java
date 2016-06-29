3613c38c691696160418c1cdc16f29bdcf4d8c25
ParisaRashidi <parisaRashidiNezhad@gmail.com>
2016-06-29 13:09:11 +0430

    recyclerView added for specialProducts sellers most shopped products articles

--- app/src/main/java/ir/rastanco/mobilemarket/presenter/MainActivity.java
+++ app/src/main/java/ir/rastanco/mobilemarket/presenter/MainActivity.java
@@ -1,15 +1,146 @@
 package ir.rastanco.mobilemarket.presenter;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import ir.rastanco.mobilemarket.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RecyclerView horizontal_recycler_view;
+    private ArrayList<String> horizontalList;
+    private HorizontalAdapter horizontalAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        horizontal_recycler_view= (RecyclerView) findViewById(R.id.horizontal_recycler_view_special_product);
+
+        horizontalList=new ArrayList<>();
+        horizontalList.add("horizontal 1");
+        horizontalList.add("horizontal 2");
+        horizontalList.add("horizontal 3");
+        horizontalList.add("horizontal 4");
+        horizontalList.add("horizontal 5");
+        horizontalList.add("horizontal 6");
+        horizontalList.add("horizontal 7");
+        horizontalList.add("horizontal 8");
+        horizontalList.add("horizontal 9");
+        horizontalList.add("horizontal 10");
+        horizontalAdapter=new HorizontalAdapter(horizontalList);
+        LinearLayoutManager verticalLayoutmanager
+                = new LinearLayoutManager(MainActivity.this, LinearLayoutManager.VERTICAL, false);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
+        LinearLayoutManager horizontalLayoutManagaer
+                = new LinearLayoutManager(MainActivity.this, LinearLayoutManager.HORIZONTAL, false);
+        horizontal_recycler_view.setLayoutManager(horizontalLayoutManagaer);
+
+        horizontal_recycler_view.setAdapter(horizontalAdapter);
+    }
+
+
+
+    public class HorizontalAdapter extends RecyclerView.Adapter<HorizontalAdapter.MyViewHolder> {
+
+        private List<String> horizontalList;
+
+        public class MyViewHolder extends RecyclerView.ViewHolder {
+            public TextView txtView;
+
+            public MyViewHolder(View view) {
+                super(view);
+                txtView = (TextView) view.findViewById(R.id.txtView);
+
+            }
+        }
+
+
+        public HorizontalAdapter(List<String> horizontalList) {
+            this.horizontalList = horizontalList;
+        }
+
+        @Override
+        public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View itemView = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.main_recyclerview_item, parent, false);
+
+            return new MyViewHolder(itemView);
+        }
+
+        @Override
+        public void onBindViewHolder(final MyViewHolder holder, final int position) {
+            holder.txtView.setText(horizontalList.get(position));
+
+            holder.txtView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(MainActivity.this,holder.txtView.getText().toString(),Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+
+        @Override
+        public int getItemCount() {
+            return horizontalList.size();
+        }
+    }
+
+
+
+    public class VerticalAdapter extends RecyclerView.Adapter<VerticalAdapter.MyViewHolder> {
+
+        private List<String> verticalList;
+
+        public class MyViewHolder extends RecyclerView.ViewHolder {
+            public TextView txtView;
+
+            public MyViewHolder(View view) {
+                super(view);
+                txtView = (TextView) view.findViewById(R.id.txtView);
+
+            }
+        }
+
+
+        public VerticalAdapter(List<String> verticalList) {
+            this.verticalList = verticalList;
+        }
+
+        @Override
+        public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View itemView = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.main_recyclerview_item, parent, false);
+
+            return new MyViewHolder(itemView);
+        }
+
+        @Override
+        public void onBindViewHolder(final MyViewHolder holder, final int position) {
+
+            holder.txtView.setText(verticalList.get(position));
+            holder.txtView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    Toast.makeText(MainActivity.this,holder.txtView.getText().toString(),Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+
+        @Override
+        public int getItemCount() {
+            return verticalList.size();
+        }
     }
 }
